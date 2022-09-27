659411e903cacd339bc3c64264b748ab74aa2f33
iMorning <morningos@foxmail.com>
2022-09-27 20:38:10 +0800

    m

--- 树/tree.h
+++ 树/tree.h
@@ -20,20 +20,20 @@ struct ParentTree {
 
 // 孩子表示法（顺序+链式存储）
 // 顺序存储各个结点，每个结点中保存孩子链表头指针
-struct ChildTreeNode
+typedef struct ChildTreeNode
 {
 	int child;           //孩子结点在数组中的位置
 	ChildTreeNode* next; //下一个孩子
-};
+}ChildTreeNode;
 struct ChildTreeBox
 {
 	ElemType data;
-	BiTreeNode* lchild, * rchild;
+	ChildTreeNode* lchild, * rchild;
 }BiTreeNode, * BiTree;
 
 // 孩子兄弟表示法（链式存储）
 typedef struct CSNode
 {
-	ElemType data; //数据域
+	ElemType data;              //数据域
 	CSNode* firstChild, * next; //第一个孩子和右兄弟指针
 }CSNode, * CSTree;
